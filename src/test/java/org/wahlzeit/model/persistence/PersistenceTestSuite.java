65813d494f4ebbf632467261ad4d5855ccdf8e1c
denizcaliskan1 <deniz.caliskan@fau.de>
2017-11-06 01:25:48 +0100

    TestSuites added for all tests.

---
+++ src/test/java/org/wahlzeit/model/persistence/PersistenceTestSuite.java
@@ -0,0 +1,11 @@
+package org.wahlzeit.model.persistence;
+
+import org.junit.runner.*;
+import org.junit.runners.*;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	org.wahlzeit.model.persistence.DatastoreAdapterTest.class
+})
+
+public class PersistenceTestSuite {/**do nothing*/}
---
+++ src/test/java/org/wahlzeit/services/mailing/MailingTestSuite.java
@@ -0,0 +1,12 @@
+package org.wahlzeit.services.mailing;
+
+import org.junit.runner.*;
+import org.junit.runners.*;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	EmailServiceTest.class
+})
+
+
+public class MailingTestSuite {/**do nothing*/}
